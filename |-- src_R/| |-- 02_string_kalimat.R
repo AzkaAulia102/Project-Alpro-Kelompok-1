231421d8777ded66846d18b26997db5ea0ce1fba
AzkaAulia102 <azka.haris@mhs.unsoed.ac.id>
2026-06-12 13:30:00 +0700

    Update | |-- 02_string_kalimat.R

--- |-- src_R/| |-- 02_string_kalimat.R
+++ |-- src_R/| |-- 02_string_kalimat.R
@@ -1,1 +1,14 @@
+K1 <- "Saya tak kan menyerah."
+K2 <- "Ia berkata, 'Aku menyayangimu'."
+K3 <- "Coba jelaskan pengertian \'cross-validation\' dalam Machine Learning!"
+K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+print(K1)
+## [1] "Saya tak kan menyerah."
+print(K2)
+## [1] "Ia berkata, 'Aku menyayangimu'."
+print(K3)
+## [1] "Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!"
+print(K4)
+## [1] "Surat keputusan itu bernomor 62/UN.34/19/2023."
 
