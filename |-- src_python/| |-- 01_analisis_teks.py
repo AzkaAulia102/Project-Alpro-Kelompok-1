27d54f1243edf1fdfaf1d6c3802c97f9c92df3fe
AzkaAulia102 <azka.haris@mhs.unsoed.ac.id>
2026-06-12 13:30:46 +0700

    Update | |-- 01_analisis_teks.py

--- |-- src_python/| |-- 01_analisis_teks.py
+++ |-- src_python/| |-- 01_analisis_teks.py
@@ -1,1 +1,7 @@
+teks = "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
+daftar_kata = teks.split()
+jumlah_kata = len(daftar_kata)
+jumlah_kalimat = teks.count(".")
+print(f"Teks tersebut memuat {jumlah_kalimat} kalimat dan {jumlah_kata} kata.")
 
+Teks tersebut memuat 5 kalimat dan 92 kata.
