3da615453031ccdb04ea35bb1c1d0ca332fb91d4
AzkaAulia102 <azka.haris@mhs.unsoed.ac.id>
2026-06-12 21:15:46 +0700

    Update | |-- 02_string_kalimat.py

--- |-- src_python/| |-- 02_string_kalimat.py
+++ |-- src_python/| |-- 02_string_kalimat.py
@@ -1,7 +1,34 @@
+#Kondisi Pertama
 K1 = "Saya tak 'kan menyerah."
 K2 = 'Ia berkata, "Aku menyayangimu."'
 K3 = '"Coba jelaskan pengertian \'cross-validation\' dalam Machine Learning!"'
 K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+print(K1)
+print(K2)
+print(K3)
+print(K4)
+
+
+
+#Kondisi Kedua
+K1 = "Pagi ini cuacanya sangat cerah."
+K2 = "Ayah berkata, 'Jangan lupa sarapan sebelum berangkat'."
+K3 = "Bisakah Anda menjelaskan konsep \'internet of things\' secara singkat?"
+K4 = "Nomor tiket perjalanan saya adalah TR-4589/2026."
+
+print(K1)
+print(K2)
+print(K3)
+print(K4)
+
+
+
+#Kondisi Ketiga
+K1 = "Membaca buku dapat menambah wawasan."
+K2 = "Dosen itu berkata, 'Kerjakan tugas dengan jujur'."
+K3 = "Apa yang dimaksud dengan \'data mining\' dalam pengolahan data?"
+K4 = "Sertifikat tersebut memiliki nomor 117/SRT/VI/2026."
 
 print(K1)
 print(K2)
