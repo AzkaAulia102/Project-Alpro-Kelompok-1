41e6630c854ada19d5e32ad22dfa55c6f806cd68
jouleputranto-dot <jouleputranto@gmail.com>
2026-06-13 23:21:37 +0700

    Update | |-- 03_akar_kuadrat.R

--- |-- src_R/| |-- 03_akar_kuadrat.R
+++ |-- src_R/| |-- 03_akar_kuadrat.R
@@ -1,21 +1,17 @@
+cat("Program Menghitung Akar Persamaan Kuadrat\n")
+
 a <- 1
 b <- -5
 c <- 6
 
-if (a == 0) {
-  cat("Nilai a tidak boleh 0 karena bukan persamaan kuadrat\n")
+diskriminan <- b^2 - 4 * a * c
+
+if (diskriminan < 0) {
+  cat("Persamaan hanya memiliki akar-akar imajiner\n")
 } else {
-  
-  diskriminan <- b^2 - 4*a*c
-  
-  if (diskriminan < 0) {
-    cat("Persamaan hanya memiliki akar-akar imajiner\n")
-  } else {
-    
-    akar1 <- (-b + sqrt(diskriminan)) / (2*a)
-    akar2 <- (-b - sqrt(diskriminan)) / (2*a)
-    
-    cat(sprintf("Akar pertama = %.3f\n", akar1))
-    cat(sprintf("Akar kedua = %.3f\n", akar2))
-  }
+  akar1 <- (-b + sqrt(diskriminan)) / (2 * a)
+  akar2 <- (-b - sqrt(diskriminan)) / (2 * a)
+
+  cat("Akar pertama =", format(akar1, nsmall = 3), "\n")
+  cat("Akar kedua =", format(akar2, nsmall = 3), "\n")
 }
