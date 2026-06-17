6d0e1cfceae6cc0b00019a90cf686fbef33cf8a2
raihannafeby227-commits <raihannafeby227@gmail.com>
2026-06-18 00:35:05 +0700

    Update | |-- 05_cluster_3d.R

--- |-- src_R/| |-- 05_cluster_3d.R
+++ |-- src_R/| |-- 05_cluster_3d.R
@@ -1,45 +1,43 @@
-A <- c(2, 1, 3)
-B <- c(1, -4, 6)
-C <- c(-2, 3, -2)
 
-U <- c(1, 2, 3)
-
-x1 <- U[1]
-x2 <- U[2]
-x3 <- U[3]
-
-jarak_A <- sqrt((x1 - A[1])^2 +
-                  (x2 - A[2])^2 +
-                  (x3 - A[3])^2)
-
-jarak_B <- sqrt((x1 - B[1])^2 +
-                  (x2 - B[2])^2 +
-                  (x3 - B[3])^2)
-
-jarak_C <- sqrt((x1 - C[1])^2 +
-                  (x2 - C[2])^2 +
-                  (x3 - C[3])^2)
-
-minimum <- min(jarak_A, jarak_B, jarak_C)
-
-jumlah_minimum <- sum(
-  c(jarak_A, jarak_B, jarak_C) == minimum
-)
-
-if (jumlah_minimum > 1) {
-  
-  cat("Tepat di Perbatasan")
-  
-} else if (minimum == jarak_A) {
-  
-  cat("Cluster A")
-  
-} else if (minimum == jarak_B) {
-  
-  cat("Cluster B")
-  
-} else {
-  
-  cat("Cluster C")
-  
+hitung_jarak <- function(titik, pusat) {
+  sqrt(
+    (titik[1] - pusat[1])^2 +
+      (titik[2] - pusat[2])^2 +
+      (titik[3] - pusat[3])^2
+  )
 }
+
+tentukan_cluster <- function(U) {
+  
+  A <- c(2, 1, 3)
+  B <- c(1, -4, 6)
+  C <- c(-2, 3, -2)
+  
+  jarak_A <- hitung_jarak(U, A)
+  jarak_B <- hitung_jarak(U, B)
+  jarak_C <- hitung_jarak(U, C)
+  
+  minimum <- min(jarak_A, jarak_B, jarak_C)
+  
+  if (sum(c(jarak_A, jarak_B, jarak_C) == minimum) > 1) {
+    return("Tepat di Perbatasan")
+  } else if (minimum == jarak_A) {
+    return("Cluster A")
+  } else if (minimum == jarak_B) {
+    return("Cluster B")
+  } else {
+    return("Cluster C")
+  }
+}
+
+U1 <- c(1, 2, 3)
+U2 <- c(1.5, -1.5, 4.5)
+
+cat("Titik", U1, "->", tentukan_cluster(U1), "\n")
+cat("Titik", U2, "->", tentukan_cluster(U2), "\n")
+
+output
+> cat("Titik", U1, "->", tentukan_cluster(U1), "\n")
+Titik 1 2 3 -> Cluster A 
+> cat("Titik", U2, "->", tentukan_cluster(U2), "\n")
+Titik 1.5 -1.5 4.5 -> Tepat di Perbatasan 
